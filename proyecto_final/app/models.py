5c4cd9e15fe7619078e687778048a1cfe49d9aff
anhiGF <153327217+anhiGF@users.noreply.github.com>
2025-04-30 20:04:41 -0600

    AdminLTE
    Esto registra Alumno en el Admin de Django, que ya usa AdminLTE

--- proyecto_final/app/models.py
+++ proyecto_final/app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Alumno(models.Model):
+    numControl = models.CharField(max_length=10)
+    nombre = models.CharField(max_length=100)
+    primerAp = models.CharField(max_length=100)
+    segundoAp = models.CharField(max_length=100)
+    fechaNac = models.DateField()
+    semestre = models.IntegerField()
+    carrera = models.IntegerField()
+
+    class Meta:
+        db_table = 'alumnos'
+        
