c8655dc4c25c8e0692153156cfb3a946086c0632
anhiGF <153327217+anhiGF@users.noreply.github.com>
2025-04-30 20:14:29 -0600

    HTML

--- proyecto_final/app/views.py
+++ proyecto_final/app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import RegistroForm
+from .models import Alumno
+from django.contrib.auth.decorators import login_required
+
+def landing(request):
+    return render(request, 'landing.html')
+
+def registro(request):
+    if request.method == 'POST':
+        form = RegistroForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = RegistroForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+@login_required
+def crud_list(request):
+    alumnos = Alumno.objects.all()
+    return render(request, 'crud_list.html', {'Alumnos': alumnos})
